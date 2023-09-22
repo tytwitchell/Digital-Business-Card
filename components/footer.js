6bfb4041c313434624ac53f9798049383dac4a9c
tytwitchell <tytwitchell@gmail.com>
2023-09-22 12:48:51 -0400

    link update

--- components/footer.js
+++ components/footer.js
@@ -3,9 +3,23 @@ import React from 'react'
 export default function Footer(props) {
     return (
         <footer style={props.stylesFooter}>
-            <img src="./images/github.png" className="social-links"/>           
-            <img src="./images/location.png" className="social-links"/>
-            <img src="./images/twitter.png" className="social-links"/>
+            <a 
+                href="https://github.com/tytwitchell" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                    <img src="./images/github.png" className="social-links"/>
+            </a>
+            <a 
+                href="https://www.google.com/maps/place/New+York,+NY/@40.6970193,-74.3093255,10z/data=!3m1!4b1!4m6!3m5!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728!16zL20vMDJfMjg2?entry=ttu" 
+                target="_blank" rel="noopener noreferrer">
+                    <img src="./images/location.png" className="social-links"/>
+            </a>
+            <a 
+                href="#" 
+                target="_blank" 
+                rel="noopener noreferrer">
+                    <img src="./images/twitter.png" className="social-links"/>
+            </a>
         </footer>
     )
 }
--- index.pack.js
+++ index.pack.js
@@ -440,9 +440,29 @@ function Footer(props) {
     return _react2.default.createElement(
         "footer",
         { style: props.stylesFooter },
-        _react2.default.createElement("img", { src: "./images/github.png", className: "social-links" }),
-        _react2.default.createElement("img", { src: "./images/location.png", className: "social-links" }),
-        _react2.default.createElement("img", { src: "./images/twitter.png", className: "social-links" })
+        _react2.default.createElement(
+            "a",
+            {
+                href: "https://github.com/tytwitchell",
+                target: "_blank",
+                rel: "noopener noreferrer" },
+            _react2.default.createElement("img", { src: "./images/github.png", className: "social-links" })
+        ),
+        _react2.default.createElement(
+            "a",
+            {
+                href: "https://www.google.com/maps/place/New+York,+NY/@40.6970193,-74.3093255,10z/data=!3m1!4b1!4m6!3m5!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728!16zL20vMDJfMjg2?entry=ttu",
+                target: "_blank", rel: "noopener noreferrer" },
+            _react2.default.createElement("img", { src: "./images/location.png", className: "social-links" })
+        ),
+        _react2.default.createElement(
+            "a",
+            {
+                href: "#",
+                target: "_blank",
+                rel: "noopener noreferrer" },
+            _react2.default.createElement("img", { src: "./images/twitter.png", className: "social-links" })
+        )
     );
 }
 
